334399825c8208289897e4b9f30c90bb8b2e058f
MUSTAFAREZA47 <mustafareza47@gmail.com>
2025-03-07 00:18:08 +0530

    Secret Auction Program

---
+++ Day_09/main.py
@@ -0,0 +1,21 @@
+print("Secret Auction Program")
+
+bids = {}
+
+bidding = True
+while bidding:
+    name = input("What is your name?: ")
+    bid = int(input("What's your bid?: $"))
+    bids[name] = bid
+    more_bids = input("Are there any other bidders? Type 'yes' or 'no'.\n")
+    if more_bids == "no":
+        bidding = False
+        for key, value in bids.items():
+            if value == max(bids.values()):
+                print(f"The winner is {key} with a bid of ${value}")
+    else:
+        continue
+
+print(bids)
+    
+    
